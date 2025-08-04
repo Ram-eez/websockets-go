17bc8665a0ab552fda5b3b3fc82f6ed355ed2f06
Ram-eez <rameez2838@gmail.com>
2025-08-04 23:27:50 +0530

    trying out new design pattern for a simple websocket chat app

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 	registerRoutes(router)
+
+	router.Run(":8080")
 }
 
 func registerRoutes(router *gin.Engine) {
 	router.GET("/", ServeIndex)
+	router.GET("/ws")
 }
 
 func ServeIndex(c *gin.Context) {
