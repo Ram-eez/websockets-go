ff95312788baa0b4a9a31ff5780948501129801c
Ram-eez <rameez2838@gmail.com>
2025-08-05 20:23:35 +0530

    using factories for managers and clients

---
+++ Manager/manager.go
@@ -0,0 +1,50 @@
+package manager
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var (
+	websocketUpgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		clients: make(ClientList),
+	}
+}
+
+func (m *Manager) ServeWS(c *gin.Context) {
+	log.Println("starting websocket new conn")
+
+	conn, err := websocketUpgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Fatal("could not upgrade conn: ", conn)
+		return
+	}
+
+	client := NewClient(conn, m)
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.clients[client] = true
+}
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	manager "websockets/Manager"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 	registerRoutes(router)
-
 	router.Run(":8080")
 }
 
 func registerRoutes(router *gin.Engine) {
+	Manager := manager.NewManager()
 	router.GET("/", ServeIndex)
-	router.GET("/ws")
+	router.GET("/ws", Manager.ServeWS)
 }
 
 func ServeIndex(c *gin.Context) {
