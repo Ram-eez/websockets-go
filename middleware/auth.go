9a390501d3b0a0d66325efc4b3d3dc2d8c70bd89
Ram-eez <rameez2838@gmail.com>
2025-08-09 15:54:10 +0530

    adding middleware functions

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,1 +1,41 @@
 package middleware
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret-key")
+
+func CreateToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		fmt.Println("err signing the jwt token")
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
+}
