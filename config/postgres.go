047a01cdfd7d7e5afed8c0506d5b72017b119bd2
Ram-eez <rameez2838@gmail.com>
2025-10-15 09:39:18 +0530

    adding postgres db connect functions

---
+++ config/postgres.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("no .env found")
+	}
+	connStr := fmt.Sprintf(
+		"user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("error opnening the DB: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to the DB: %w", err)
+	}
+	fmt.Println("Database conn established successfully")
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"websockets/config"
 	"websockets/routes"
 
 	"github.com/gin-gonic/gin"
@@ -8,6 +10,11 @@ import (
 
 func main() {
 	router := gin.Default()
+	conn, err := config.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
 	routes.RegisterRoutes(router)
 	router.LoadHTMLGlob("views/*.html")
 	router.Run(":8080")
